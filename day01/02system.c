a91d0d4ad3fae834e0f2e84390e6c3c17050c858
CANAAN-Tan <18200132258@163.com>
2021-01-31 15:02:04 +0800

    day1完成

--- day01/02system.c
+++ day01/02system.c
@@ -7,7 +7,7 @@ int main(void)
 	//也可以打开一个外部的应用程序，如果带路径，则需要带上双斜杠  或者使用一个正斜杠
 	//system("D:\\Desktop\\feiq.exe")
 	int value;
-	value = system("calc"); 
+	value = system("calc");
 	//%d是一个占位符，表示输出一个整型数据
 	printf("%d\n", value);
 	return 0;
